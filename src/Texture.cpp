ae95ad9095913edf239d0106fac1bae984945227
Dane Larsen <dane.t.larsen@gmail.com>
2012-12-31 12:52:27 -0700

    changed texture nearest to linear

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -17,8 +17,8 @@ GLuint Texture::loadTexture(const char* img_path)
         glBindTexture(GL_TEXTURE_2D, texture);
 	    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img->GetWidth(), img->GetHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, img->GetPixelsPtr());
 	    glBindTexture(GL_TEXTURE_2D, 0);
 	    return texture;
